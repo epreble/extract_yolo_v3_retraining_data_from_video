622e4af83947072ce83e25c85dc030bec0dd4ea2
epreble <epreble@rti.org>
2019-11-23 17:17:04 -0500

    test mode functioning with frame resize

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ Use object tracking software (OpenCV2) to generate yolov3 suitable training imag
 
 - Obtain videos of the objects of interest
 - `test` each video
-    - Run `python Object_Tracker_for_Video.py test <video_path>`
+    - Run `python object_track_extract_yolov3.py test <video_path>`
     - The first video frame is loaded
     - Draw a bounding box around the object of interest
     - Press the spacebar and verify that the bounding box properly follows the object without errors
         - If errors occur, change the `tracker_types` in the script to another type and try again
 - `capture` data from each video
-    - Run `python object_tracker_for_video.py capture <video_path> <output_path>`
+    - Run `python object_track_extract_yolov3.py capture <video_path> <output_path>`
     - Follow the same process as above.
     - With the 'capture' argument, data will be output to the `<output path>`
 
--- src/object_track_extract_yolov3.py
+++ src/object_track_extract_yolov3.py
@@ -6,32 +6,101 @@ import cv2
 import sys
 
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+print(major_ver, minor_ver, subminor_ver)
+
+
+def process_args(args):
+    mode = 'undefined'
+    video_path = 'undefined'
+    output_path = 'undefined'
+
+    try:
+        mode = args[1]
+        print()
+        print('mode:', mode)
+    except:
+        print()
+        print('Error')
+        print('mode argument missing. <test> or <capture> required as arg0')
+        sys.exit()
+
+    if mode == 'test':
+        try:
+            video_path = args[2]
+            print('video_path:', video_path)
+        except:
+            print()
+            print('Error')
+            print('video path must be supplied as argument')
+            sys.exit()
+    elif mode == 'capture':
+        try:
+            video_path = args[2]
+            output_path = args[3]
+            print('video_path:', video_path)
+            print('output_path:', output_path)
+        except:
+            print()
+            print('Error')
+            print('video path and output path must be supplied as arguments')
+            sys.exit()
+
+    return mode, video_path, output_path
+
+
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    # print('h:', h)
+    # print('w:', w)
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter), r
+
 
 if __name__ == '__main__':
 
+    # python object_track_extract_yolov3.py test <video_path>
+    # python object_track_extract_yolov3.py capture <video_path> <output_path>
+
+    mode, video_path, output_path = process_args(sys.argv)
+
+    print()
+    # sys.exit()
     # Set up tracker. Choose type from list.
 
     tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN']
-    tracker_type = tracker_types[0]
+    tracker_type = tracker_types[1]
 
-    if int(minor_ver) < 3:
-        tracker = cv2.Tracker_create(tracker_type)
-    else:
-        if tracker_type == 'BOOSTING':
-            tracker = cv2.TrackerBoosting_create()
-        if tracker_type == 'MIL':
-            tracker = cv2.TrackerMIL_create()
-        if tracker_type == 'KCF':
-            tracker = cv2.TrackerKCF_create()
-        if tracker_type == 'TLD':
-            tracker = cv2.TrackerTLD_create()
-        if tracker_type == 'MEDIANFLOW':
-            tracker = cv2.TrackerMedianFlow_create()
-        if tracker_type == 'GOTURN':
-            tracker = cv2.TrackerGOTURN_create()
+    # if int(minor_ver) < 3:
+    #     tracker = cv2.Tracker_create(tracker_type)
+    # else:
+    if tracker_type == 'BOOSTING':
+        tracker = cv2.TrackerBoosting_create()
+    if tracker_type == 'MIL':
+        tracker = cv2.TrackerMIL_create()
+    if tracker_type == 'KCF':
+        tracker = cv2.TrackerKCF_create()
+    if tracker_type == 'TLD':
+        tracker = cv2.TrackerTLD_create()
+    if tracker_type == 'MEDIANFLOW':
+        tracker = cv2.TrackerMedianFlow_create()
+    if tracker_type == 'GOTURN':
+        tracker = cv2.TrackerGOTURN_create()
+    if tracker_type == 'MOSSE':
+        tracker = cv2.TrackerMOSSE_create()
+    if tracker_type == "CSRT":
+        tracker = cv2.TrackerCSRT_create()
 
     # Read video
-    video = cv2.VideoCapture("path_to_video.mov")
+    video = cv2.VideoCapture(video_path)
 
     # Exit if video not opened.
     if not video.isOpened():
@@ -49,22 +118,29 @@ if __name__ == '__main__':
     # bbox = (1393, 1485, 1443, 1535)
 
     # Uncomment the line below to select a different bounding box
-    bbox = cv2.selectROI(frame, False)
+    # (h, w) = frame.shape[:2]
+    resize, ratio = ResizeWithAspectRatio(frame, width=500)
+    # print('ratio', ratio)
+    bbox = cv2.selectROI(resize, False)
 
     # Initialize tracker with first frame and bounding box
-    ok = tracker.init(frame, bbox)
+    ok = tracker.init(resize, bbox)
 
     while True:
         # Read a new frame
         ok, frame = video.read()
         if not ok:
             break
+        resize, ratio = ResizeWithAspectRatio(frame, width=500)  # Resize by width OR
 
         # Start timer
         timer = cv2.getTickCount()
 
         # Update tracker
-        ok, bbox = tracker.update(frame)
+        ok, bbox = tracker.update(resize)
+
+        # print(bbox)
+        # sys.exit()
 
         # Calculate Frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
@@ -74,21 +150,25 @@ if __name__ == '__main__':
             # Tracking success
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            cv2.rectangle(resize, p1, p2, (255, 0, 0), 2, 1)
+            # if mode == 'capture':
+            #     cv2.imwrite(os.path.join(output_path, '%d.jpg') % count, resize)
+
         else:
             # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            cv2.putText(resize, "Tracking failure detected", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        # sys.exit()
 
         # print(frame)
 
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        cv2.putText(resize, tracker_type + " Tracker", (100, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        cv2.putText(resize, "FPS : " + str(int(fps)), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
 
         # Display result
-        cv2.imshow("Tracking", frame)
+        cv2.imshow("Tracking", resize)
 
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
